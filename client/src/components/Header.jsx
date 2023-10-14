4471e46ae1e7dc1ea5e6d81db8181a8fcfce7d1c
jasurbek <jasurdev1604@gmail.com>
2023-10-14 17:48:52 +0500

    Routing

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,10 +4,10 @@ import { Outlet } from "react-router-dom";
 const Header = () => {
   return (
     <div>
-      <div className="bg-slate-200">
-        <div className="flex justify-between items-senter max-w-6xl mx-auto p-3">
-          <h1 className="font-bold">Auth App</h1>
-          <ul className="flex gap-4">
+      <div>
+        <div>
+          <h1>Auth App</h1>
+          <ul>
             <li>Home</li>
             <li>About</li>
             <li>Sign In</li>
