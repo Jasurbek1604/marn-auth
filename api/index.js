0c33bbc4cc4b2016a5dc69f2fe47d79c1a6cbf81
jasurbek <jasurdev1604@gmail.com>
2023-11-03 22:36:27 +0500

    Create signup aip route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,8 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
+
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
@@ -12,8 +14,11 @@ mongoose
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, () => {
   console.log("Server listening on port 3000");
 });
 
 app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
