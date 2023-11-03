fd1f6e277d6401a598edbbb13d29def0dc32a3b4
jasurbek <jasurdev1604@gmail.com>
2023-11-04 00:41:45 +0500

    complate signup page ui

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import { Link } from "react-router-dom";
+import { NavLink as Link } from "react-router-dom";
 
 const Header = () => {
   return (
@@ -8,17 +8,32 @@ const Header = () => {
       <div className="bg-slate-200">
         <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
           <Link to="/">
-            <h1 className="font-bold">Auth App</h1>
+            <h1 className="font-bold text-2xl">Auth App</h1>
           </Link>
           <ul className="flex gap-4">
-            <Link to="/">
-              <li>Home</li>
+            <Link
+              to="/"
+              className={({ isActive }) =>
+                isActive ? "text-black" : "text-gray-600"
+              }
+            >
+              <li className="text-xl">Home</li>
             </Link>
-            <Link to="/about">
-              <li>About</li>
+            <Link
+              to="/about"
+              className={({ isActive }) =>
+                isActive ? "text-black" : "text-gray-600"
+              }
+            >
+              <li className="text-xl">About</li>
             </Link>
-            <Link to="/signin">
-              <li>Sign In</li>
+            <Link
+              to="/signin"
+              className={({ isActive }) =>
+                isActive ? "text-black" : "text-gray-600"
+              }
+            >
+              <li className="text-xl">Sign In</li>
             </Link>
           </ul>
         </div>
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 const SignUp = () => {
   return (
-    <div>SignUp</div>
-  )
-}
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
+      <form action="" className="flex flex-col gap-4">
+        <input
+          type="text"
+          placeholder="Username"
+          id="username"
+          className="bg-slate-100 p-3 rounded-lg"
+        />
+        <input
+          type="email"
+          placeholder="Emeil"
+          id="email"
+          className="bg-slate-100 p-3 rounded-lg"
+        />
+        <input
+          type="password"
+          placeholder="password"
+          id="password"
+          className="bg-slate-100 p-3 rounded-lg"
+        />
+        <button className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80">
+          Sing Up
+        </button>
+      </form>
+      <div className="flex gap-2 mt-5">
+        <p>Have an account? </p>
+        <NavLink to="/singin">
+          <span className="text-blue-500">Sing In</span>
+        </NavLink>
+      </div>
+    </div>
+  );
+};
 
-export default SignUp
+export default SignUp;
