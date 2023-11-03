e2ee5212206823cf496969ebd04647f2fc4fcd3f
jasurbek <jasurdev1604@gmail.com>
2023-11-04 00:57:10 +0500

    complate signup page ui

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -10,22 +10,25 @@ const SignUp = () => {
           type="text"
           placeholder="Username"
           id="username"
-          className="bg-slate-100 p-3 rounded-lg"
+          className="bg-slate-100 p-3 outline-none rounded-lg"
         />
         <input
           type="email"
           placeholder="Emeil"
           id="email"
-          className="bg-slate-100 p-3 rounded-lg"
+          className="bg-slate-100 p-3 outline-none rounded-lg"
         />
         <input
           type="password"
           placeholder="password"
           id="password"
-          className="bg-slate-100 p-3 rounded-lg"
+          className="bg-slate-100 outline-none p-3 rounded-lg"
         />
         <button className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80">
           Sing Up
+        </button>
+        <button className="bg-red-600 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80">
+          Continue with google
         </button>
       </form>
       <div className="flex gap-2 mt-5">
